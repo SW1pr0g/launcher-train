0d2d8fe463b2af1ce2619934be65d94e3098ccc9
SW1pr0g <poryadin_alex@mail.ru>
2022-12-21 16:26:42 +0300

    Creating branch

---
+++ launcher-train/View/LoginView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace launcher_train.View
+{
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+    }
+}
