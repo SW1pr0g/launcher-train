470cfcbb5391770700237352564d9d3a09ad749a
SW1pr0g <poryadin_alex@mail.ru>
2022-12-21 14:10:50 +0300

    Fixing project

---
+++ launcher-train/windowmain.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace launcher_train
+{
+    public partial class windowmain : Window
+    {
+        public windowmain()
+        {
+            InitializeComponent();
+        }
+    }
+}
