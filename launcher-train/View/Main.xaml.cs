cbeef6c40f559c7677b0e07931f03740efe4a4cd
SW1pr0g <poryadin_alex@mail.ru>
2022-12-21 16:22:27 +0300

    Last fix

---
+++ launcher-train/View/Main.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace launcher_train.View
+{
+    public partial class Main : Window
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
