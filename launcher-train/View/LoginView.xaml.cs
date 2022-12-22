dc556b2e4ec6ec1b729189df19e36232c4e3cf8e
SW1pr0g <poryadin_alex@mail.ru>
2022-12-22 23:16:43 +0300

    Refactor xaml code, add closing message, icon

--- launcher-train/View/LoginView.xaml.cs
+++ launcher-train/View/LoginView.xaml.cs
@@ -5,9 +5,21 @@ namespace launcher_train.View
 {
     public partial class LoginView : Window
     {
+        private bool _AuthClosed = false;
         public LoginView()
         {
             InitializeComponent();
+        }
+
+        private void LoginView_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (_AuthClosed == false)
+            {
+                var res = MessageBox.Show("Вы действительно хотите выйти из приложения?", "Выход из приложения", 
+                    MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (res == MessageBoxResult.No)
+                    e.Cancel = true;
+            }
         }
 
         private void LoginView_OnMouseDown(object sender, MouseButtonEventArgs e)
@@ -29,6 +41,6 @@ namespace launcher_train.View
         private void BtnLogin_OnClick(object sender, RoutedEventArgs e)
         {
             throw new System.NotImplementedException();
-        }
+        }        
     }
 }
